c68205afaf3b321ac37eaa9da92f11d764e7fa21
Mikeogod <mikeogod@gmail.com>
2013-10-15 17:40:30 -0400

    commit

--- 3-a2.js
+++ 3-a2.js
@@ -9,3 +9,17 @@
     };
  
 }( jQuery ));
+
+//Canvas manipulation
+var circle=document.getElementById("circle");
+var con=circle.getContext("2d");
+
+var fillColor="";
+//
+fillColor="red";
+//
+con.fillStyle=fillColor;
+con.beginPath();
+con.arc(75, 75, 60, 0, 2*Math.PI);
+con.stroke();
+con.fill();
