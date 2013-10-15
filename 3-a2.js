a54aa4ac6a8f80f122821676d334e4c623cd1186
tmehdi94 <mehdit@rpi.edu>
2013-10-15 17:38:05 -0400

    Update 3-a2.js

--- 3-a2.js
+++ 3-a2.js
@@ -1,11 +1,19 @@
-(function( $ ) {
- 
-    $.fn.showLinkLocation = function() {
- 
-        return this.filter( "a" ).append(function() {
-            return " (" + this.href + ")";
-        });
- 
-    };
- 
-}( jQuery ));
+$( document ).ready(function() {
+	setUpGame
+	$( "span#randomcolor" ).html( getRandomColor() );
+});
+
+function getRandomColor() {
+	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	return color;
+}
+
+function convertDecToHex(var dec) {
+	var hex = dec.toString(16);
+	return hex;
+}
+
+function convertHexToDec(var hex) {
+	var dec = parseInt(hex,16);
+	return dec;
+}
