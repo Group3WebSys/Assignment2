2f94b7116ebbd859a2eb62bb3750c8a15945a755
Hydriad <schluh@rpi.edu>
2013-10-21 20:04:46 -0400

    Update 3-a2.js

--- 3-a2.js
+++ 3-a2.js
@@ -83,9 +83,6 @@ $( document ).ready(function() {
 	//Hide Play again!
 	$("#again[type='type='submit']").button("disable");
 	//Click event handler for "Play again!"
-	//$("#again").click(function(e){
-	//	$("#game").show();
-	//});
 	$('#again').click(function(e) {
     	location.reload();
 	});
